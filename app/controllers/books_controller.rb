0e2b7d9f0e9417bddd3a9d5cf1126890ea727e04
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-07-28 13:26:23 +0200

    Add spring

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,10 @@ class BooksController < ApplicationController
 
   def index
       books = Book.all
+
+      if rating = params[:rating]
+        books = books.where(rating: rating)
+      end
       render json: books, status: 200
   end
 
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -4,7 +4,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 
   setup do
    Book.create!(title: 'Pragmatic Programmer', rating: 5)
-   Book.create!(title: 'Ender´s Game', rating: 5)
+   Book.create!(title: 'Ender´s Game', rating: 4)
   end
 
   test 'linsting books' do
@@ -14,4 +14,13 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal Mime::JSON, response.content_type
     assert_equal Book.count, JSON.parse(response.body).size
   end
+
+  test 'lists top rated books' do
+    get '/books?rating=5'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    assert_equal 1, JSON.parse(response.body).size
+  end
+
 end
